2c352f7598eb0756295a71b227e0665d4b66ff74
dmitrii.kalenitskii <dogee4803@gmail.com>
2024-08-22 21:03:22 +0800

    Finished logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,11 +38,21 @@ class AuthController extends Controller
         if (Auth::attempt($credentials, $request->remember)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('/');
+            return redirect()->intended('/dashboard');
         }
 
         return back()->withErrors([
             'email' => 'Введены неправильные почта и/или пароль.',
         ])->onlyInput('email');
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+ 
+        $request->session()->invalidate();
+     
+        $request->session()->regenerateToken();
+     
+        return redirect('/');
     }
 }
--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,9 +1,8 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
     <Head>
-        <title>| {{$page.component}}</title>
+        <title>| {{ $page.component }}</title>
         <meta head-key="description" name="description" content="Описание" />
     </Head>
     <div>
@@ -12,15 +11,28 @@
                 <div class="space-x-6">
                     <Link :href="route('home')" class="nav-link">Home</Link>
                 </div>
-
-                <div class="space-x-6">
-                    <Link :href="route('login')" class="nav-link">Sign-in</Link>
+                <div v-if="$page.props.auth.user" class="space-x-6">
+                    <Link :href="route('dashboard')" class="nav-link">
+                        Профиль
+                    </Link>
+                    <Link
+                        :href="route('logout')"
+                        method="post"
+                        as="button"
+                        type="button"
+                        class="nav-link"
+                        >Выйти</Link
+                    >
+                </div>
+                <div v-else class="space-x-6">
+                    <Link :href="route('login')" class="nav-link">Войти</Link>
                 </div>
             </nav>
         </header>
 
         <main class="p-4">
-            <slot /> <!-- Позволяет вставлять содержимое в основной компонент -->
+            <slot />
+            <!-- Позволяет вставлять содержимое в основной компонент -->
         </main>
     </div>
 </template>
